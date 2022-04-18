6da9a498e93fa0d9b5c7a0518672c9476bab3d83
MachineLearningAmateur <jzhan326@ucr.edu>
2022-04-18 03:13:53 -0700

    timer and ai mode implemented

--- Lab2/main.js
+++ Lab2/main.js
@@ -14,9 +14,14 @@ window.addEventListener("load", (event) => { //need this listener to put the scr
     //buttons
     var ngButton = document.getElementsByClassName("new_game");
     var resetButton = document.getElementsByClassName("reset");
+    var toggleButton = document.getElementById("mode");
 
+    var results = document.getElementById("results");
+
+    toggleButton.addEventListener('click', toggleMode);
     ngButton[0].addEventListener('click', newGame);
     resetButton[0].addEventListener('click', resetGame);
+
     spot1.addEventListener('click', choice);
     spot2.addEventListener('click', choice);
     spot3.addEventListener('click', choice);
@@ -33,17 +38,24 @@ var player1Score = 0;
 var player2Score = 0;
 var dict = {"one" : [0,0], "two" : [1, 0], "three" : [2, 0], "four" : [0, 1], "five" : [1,1], "six" : [2,1], "seven" : [0,2], "eight" : [1, 2], "nine" : [2,2]};
 var gameBoard = [['', '', ''], ['', '', ''], ['', '', '']]; //3 by 3 matrix
-var variables = ['']
 
+var newTimer;
+var countDown;
+var secs = 5;
+var aiMode = false;
+var winner = false;
 
 //updates the game board in real time
 function choice(event) { 
+    clearTimeout(newTimer);
     let slot = event.currentTarget.className;
     //console.log("something: ", slot);
     let currChoice = event.currentTarget.firstChild;
-    if (currChoice.innerHTML != '') {
+
+    if (currChoice.innerHTML != '' || winner) {
         return 
     }
+
     if (playerTurn%2  == 1) {
         currChoice.innerHTML = "X";
         gameBoard[dict[slot][0]][dict[slot][1]] = "X";
@@ -55,34 +67,145 @@ function choice(event) {
     }
     
     playerTurn += 1;
-
+    if (aiMode) {
+        //do something
+        aiChoice();
+    }
     //checkWinCon()
-    //start timer?
+    newTimer = setTimeout(skipTurn, 5000); 
+    clearTimeout(countDown);
+    secs = 5;
+    updateCountDown(); //starts the timer
+
+    if (isGameBoardFull()) {
+        results.innerHTML = "Match Result: It's a tie!"
+        clearTimeout(countDown);
+        clearTimeout(newTimer);
+        var el = document.getElementById("timer");
+        el.innerHTML = "Timer Countdown: 0";
+    }
 }
 
+function aiChoice() {
+    //do ai stuff
+    document.getElementsByClassName("display_player")[0].innerHTML = "X";
+    var i = Math.floor(Math.random() * 3);
+    var j = Math.floor(Math.random() * 3);
+    playerTurn += 1;
+    if (isGameBoardFull()) {
+        clearTimeout(countDown);
+        clearTimeout(newTimer);
+        var el = document.getElementById("timer");
+        el.innerHTML = "Timer Countdown: 0";
+        return;
+    }
+    while (true) {
+        console.log(i, j);
+        if (gameBoard[i][j] == '') {
+            gameBoard[i][j] = 'O';
+            Object.entries(dict).forEach(([key, value]) => {
+                console.log(value);
+                if (value[0] == i && value[1] == j){
+                    console.log(key);
+                    document.getElementsByClassName(key)[0].firstChild.innerHTML = "O";
+                }
+            });
+            console.log(gameBoard);
+            break;
+        } else {
+            i = Math.floor(Math.random() * 3);
+            j = Math.floor(Math.random() * 3);
+        }
+    }
+}
+
+//checks if gameBoard is filled
+function isGameBoardFull() {
+    var filled = 0;
+    for (var i = 0; i < gameBoard.length; i++) {
+        for (var j = 0; j < gameBoard[i].length; j++) {
+            if (gameBoard[i][j] != '') {
+                filled += 1;
+            }
+        }
+    }
+
+    if (filled == 9) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//updates the countdown for skipping player's turn
+function updateCountDown() {
+    var el = document.getElementById("timer");
+    el.innerHTML = "Timer Countdown: " + secs;
+    secs--;
+    countDown = setTimeout(updateCountDown, 1000);
+}
+
+//skips player's turn when countdown is met
+function skipTurn() {
+    alert("Current Player " + (playerTurn%2 == 1 ? 'X' : 'O') + "'s turn has been skipped!");
+    playerTurn += 1;
+    document.getElementsByClassName("display_player")[0].innerHTML = playerTurn%2 == 1 ? 'X' : 'O';
+    if (aiMode) {
+        aiChoice();
+    }
+    newTimer = setTimeout(skipTurn, 5000); 
+    secs = 5;
+}
+
+//clears the board that is being displayed on the webpage
 function clearBoard() {
     let choices = document.getElementsByClassName("xo");
     for (var i = 0; i < choices.length; i++) {
         console.log(choices[i]);
         choices[i].innerHTML = "";
     }
+    results.innerHTML = "Match Result: &nbsp &nbsp &nbsp &nbsp &nbsp";
 }
 
-function newGame() {// if new game button is pressed, resets the board but keeps the score
+// if new game button is pressed, resets the board but keeps the score
+function newGame() {
     gameBoard = [['', '', ''], 
                 ['', '', ''], 
                 ['', '', '']]; 
     clearBoard();
+    playerTurn = 1;
+    clearTimeout(newTimer);
+    clearTimeout(countDown);
+    var el = document.getElementById("timer");
+    el.innerHTML = "Timer Countdown: 0";
+    document.getElementsByClassName("display_player")[0].innerHTML = playerTurn%2 == 1 ? 'X' : 'O';
 }
 
-function resetGame () { //if reset button is pressed, resets the board and score
+//if reset button is pressed, resets the board and score
+function resetGame () { 
     //clear board and score
     player1Score = 0
     player2Score = 0
     newGame();
+    aiMode = true;
+    toggleMode();
     alert("Game has been reset!");
 }
 
+//toggles the current game mode from player to ai or vice versa
+function toggleMode() {
+    aiMode = !aiMode; //toggles the opposite
+    var displayMode = document.getElementById("modeDisplay");
+    newGame(); //to avoid breaking the game, just start a new game if game mode is toggled.
+    if (aiMode) {
+        displayMode.innerHTML = "Current Mode: Player against AI";
+    } else {
+        displayMode.innerHTML = "Current Mode: Player against Player";
+    }
+
+}
+
+//checks if the win condition is met
 function checkWinCon() {
     //need to implement | increment the score of the player who won
     if ((gameBoard[0][0] == gameBoard[0][1] == gameBoard[0][2]) || 
