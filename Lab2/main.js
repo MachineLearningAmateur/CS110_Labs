04308d83ad37289ee6e961a8ffb1172b343e39d3
MachineLearningAmateur <jzhan326@ucr.edu>
2022-04-17 21:42:30 -0700

    clicks work now!

--- Lab2/main.js
+++ Lab2/main.js
@@ -1,29 +1,29 @@
 window.addEventListener("load", (event) => { //need this listener to put the script within the header, otherwise put the script at the end
     console.log("Page fully loaded"); 
+    const spot1 = document.querySelector('.one');
+    const spot2 = document.querySelector('.two');
+    const spot3 = document.querySelector('.three');
+    const spot4 = document.querySelector('.four');
+    const spot5 = document.querySelector('.five');
+    const spot6 = document.querySelector('.six');
+    const spot7 = document.querySelector('.seven');
+    const spot8 = document.querySelector('.eight');
+    const spot9 = document.querySelector('.nine');
+
+    spot1.addEventListener('click', (event) => {
+        alert("hello");
+    });
     //get the elements needed
 });
 
-var playerTurn = 0
-var player1Score = 0
-var player2Score = 0
+var playerTurn = 1; //player 1 is X and player 2 is O
+var player1Score = 0;
+var player2Score = 0;
 var gameBoard = [['', '', ''], ['', '', ''], ['', '', '']]; //3 by 3 matrix
-
-const spot1 = document.querySelector('one');
-const spot2 = document.querySelector('two');
-const spot3 = document.querySelector('three');
-const spot4 = document.querySelector('four');
-const spot5 = document.querySelector('five');
-const spot6 = document.querySelector('six');
-const spot7 = document.querySelector('seven');
-const spot8 = document.querySelector('eight');
-const spot9 = document.querySelector('nine');
-
 
 
 function gameStart() { //if reset button is pressed start game, assuming 2 players
-    playerTurn = 0
-    player1Score = 0
-    player2Score = 0
+    playerTurn = 1
     gameBoard = [['', '', ''], ['', '', ''], ['', '', '']];
 }
 
@@ -36,6 +36,10 @@ function updateBoard() {
     //update game board display
 }
 
+function clearBoard() {
+
+}
+
 function newGame() {// if new game button is pressed, resets the board but keeps the score
     gameBoard = [['', '', ''], 
                 ['', '', ''], 
@@ -44,6 +48,8 @@ function newGame() {// if new game button is pressed, resets the board but keeps
 
 function resetGame () { //if reset button is pressed, resets the board and score
     //clear board and score
+    player1Score = 0
+    player2Score = 0
     gameStart();
 }
 
@@ -58,7 +64,7 @@ function checkWinCon() {
     (gameBoard[1][1] == gameBoard[2][2] == gameBoard[3][3]) || 
     (gameBoard[1][3] == gameBoard[2][2] == gameBoard[3][1])
     ) {
-        if (playerTurn % 2 == 0) { // player 1 won
+        if (playerTurn % 2 == 1) { // player 1 won
             player1Score++;
         }
         else {
