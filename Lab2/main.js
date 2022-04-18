1fab636ed803c50af93ea4eb75ea0d8a3461a5f5
Jason Chang <jchan149@ucr.edu>
2022-04-17 20:09:15 -0700

    added check win con

--- Lab2/main.js
+++ Lab2/main.js
@@ -8,15 +8,15 @@ var player1Score = 0
 var player2Score = 0
 var gameBoard = [['', '', ''], ['', '', ''], ['', '', '']]; //3 by 3 matrix
 
-const spot1 = document.getElementById('one');
-const spot2 = document.getElementById('two');
-const spot3 = document.getElementById('three');
-const spot4 = document.getElementById('four');
-const spot5 = document.getElementById('five');
-const spot6 = document.getElementById('six');
-const spot7 = document.getElementById('seven');
-const spot8 = document.getElementById('eight');
-const spot9 = document.getElementById('nine');
+const spot1 = document.querySelector('one');
+const spot2 = document.querySelector('two');
+const spot3 = document.querySelector('three');
+const spot4 = document.querySelector('four');
+const spot5 = document.querySelector('five');
+const spot6 = document.querySelector('six');
+const spot7 = document.querySelector('seven');
+const spot8 = document.querySelector('eight');
+const spot9 = document.querySelector('nine');
 
 
 
@@ -27,12 +27,7 @@ function gameStart() { //if reset button is pressed start game, assuming 2 playe
 
 function getInput () {
     //check for player turn | use % to calculate turn | even is player X | odd is player O, get input and set board status
-    if (playerTurn % 2 == 0 ) { // if player 1's turn
-        
-    }
-    else { //if player 2's turn
-
-    }
+    
 }
 
 function updateBoard() {
@@ -40,7 +35,9 @@ function updateBoard() {
 }
 
 function newGame() {// if new game button is pressed, resets the board but keeps the score
-    gameBoard = [['', '', ''], ['', '', ''], ['', '', '']]; 
+    gameBoard = [['', '', ''], 
+                ['', '', ''], 
+                ['', '', '']]; 
 }
 
 function resetGame () { //if reset button is pressed, resets the board and score
@@ -50,8 +47,18 @@ function resetGame () { //if reset button is pressed, resets the board and score
 
 function checkWinCon() {
     //need to implement | increment the score of the player who won
-    if ()
-    console.log("we have a winner ...")
+    if ((gameBoard[1][1] == gameBoard[1][2] == gameBoard[1][3]) || 
+    (gameBoard[2][1] == gameBoard[2][2] == gameBoard[2][3]) ||
+    (gameBoard[3][1] == gameBoard[3][2] == gameBoard[3][3]) ||
+    (gameBoard[1][1] == gameBoard[2][1] == gameBoard[3][1]) ||
+    (gameBoard[1][2] == gameBoard[2][2] == gameBoard[3][2]) || 
+    (gameBoard[1][3] == gameBoard[2][3] == gameBoard[3][3]) || 
+    (gameBoard[1][1] == gameBoard[2][2] == gameBoard[3][3]) || 
+    (gameBoard[1][3] == gameBoard[2][2] == gameBoard[3][1])
+    ) {
+        console.log("GAME OVER");
+    }
+    console.log("checking ...")
 }
 
 
