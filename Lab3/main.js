59efb005f081f5559e1f02a520c2bf38442beda0
MachineLearningAmateur <jzhan326@ucr.edu>
2022-04-24 18:16:50 -0700

    something to start with

--- Lab3/main.js
+++ Lab3/main.js
@@ -3,12 +3,13 @@
 let nInterval;
 
 function updateFeed() {
-    if (!nInterval)
-    nInterval = setInterval(pullFeed, 5000);    
+    if (!nInterval) {
+        nInterval = setInterval(pullFeed, 5000);    
+    }
 }
 
 function pullFeed() {
-    const url = "<endpoint>/feed/random?q=weather"
+    const url = "ec2-18-209-247-77.compute-1.amazonaws.com:3000/feed/random?q=weather"
 
     fetch(url)
        .then(res => res.json()) .then(data => {  
