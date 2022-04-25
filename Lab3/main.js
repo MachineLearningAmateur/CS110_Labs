1a7c9b45e03ef16c88d44aeb1351096f3b49fbf8
Jason Chang <jchan149@ucr.edu>
2022-04-24 20:12:47 -0700

    added classes

--- Lab3/main.js
+++ Lab3/main.js
@@ -1,6 +1,34 @@
 // specify a url, in this case our web server
 
-let nInterval;
+var serverURL = "ec2-18-209-247-77.compute-1.amazonaws.com:3000/"
+
+var feed = [];
+
+var nInterval;
+
+
+class Post {
+    constructor(id) {
+        this.id = id;
+        this.author = null;
+        this.date = null;
+        this.image = null;
+        this.text = null;
+        this.next = null;
+      }
+      
+}
+
+class PostFeed {
+    constructor(head) {
+        this.head = head;
+        this.length = null;
+      }
+      push () {
+          
+      }
+}
+
 
 function updateFeed() {
     if (!nInterval) {
@@ -14,9 +42,17 @@ function pullFeed() {
     fetch(url)
        .then(res => res.json()) .then(data => {  
        // do something to parse data
-    
+        
     })
     .catch(err => {
         // error catching
     console.log(err) }) 
 }
+
+let searchString = "" // here we use a global variable
+
+const handleSearch = event => {
+    searchString = event.target.value.trim().toLowerCase();
+    // you may want to update the displayed HTML here too
+}
+document.getElementById("searchBar").addEventListener("input", handleSearch)
